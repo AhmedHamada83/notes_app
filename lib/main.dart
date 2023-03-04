90d3ba8a250b6b9cc88ad49f671a8f0cdb7cdc87
AhmedHamada83 <66108343+AhmedHamada83@users.noreply.github.com>
2023-03-05 01:36:52 +0200

    change theme data

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Scaffold(),
+    );
   }
 }
